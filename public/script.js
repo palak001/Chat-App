0cdd26170199d0f43233ffaeec6810b7ade0fe9d
palak001 <palak7372@gmail.com>
2019-12-27 18:02:52 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -3,19 +3,31 @@ window.onload = function(){
   var socket = io.connect('http://localhost:4000');
 
   // query dom
-  var txt = document.getElementById('m');
-  var btn = document.getElementById('btn');
-  var messages = document.getElementById('messages');
+  var output = document.getElementById('output');
+  var send = document.getElementById('send');
+  var message = document.getElementById('message');
+  var typing = document.getElementById('typing');
+  var handle = document.getElementById('handle');
   //emit events
-  btn.addEventListener('click', function(e){
+
+  send.addEventListener('click', function(e){
     e.preventDefault(); // prevents page reloading
-    socket.emit('chat', {txt: txt.value});
-    txt.value="";
+    socket.emit('chat', {message: message.value, handle: handle.value});
+    message.value="";
+  });
+
+  message.addEventListener('keypress', function(){
+    socket.emit('typing', handle.value);
   });
 
   //listen for events
   socket.on('chat', function(data){
     console.log(data.value);
-    messages.innerHTML += '<li>' + data.txt;
+    typing.innerHTML = "";
+    output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+  });
+  //listen for typing
+  socket.on('typing', function(data){
+    typing.innerHTML = '<p><em>' + data + ' is typing </em></p>';
   });
 };
